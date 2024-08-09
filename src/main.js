a0268741187bff298e0c1ec23c5c993278fe98b4
Максим Прядкин <120360513+Lahma-tut@users.noreply.github.com>
2024-08-09 17:06:04 +0300

    update link, commit 2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/Aura'
+import Tooltip from 'primevue/tooltip'
 import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+}).directive('tooltip', Tooltip)
+
+app.mount('#app')

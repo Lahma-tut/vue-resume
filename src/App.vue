029b22db64132372429ca5967ae113da047dba3f
Максим Прядкин <120360513+Lahma-tut@users.noreply.github.com>
2024-08-09 12:03:34 +0300

    commit-1

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div class="content">
+        <div class="content-top">
+          <h1>Максим Прядкин</h1>
+          <p>Фронтенд разработчик, Vue.js</p>
+        </div>
+        <div class="content-bottom">
+          <div class="link-telegram">
+            <img src="/src/assets/telegram-logo-thin-191716.svg" alt="telegram" width="33">
+            <a href="/">maxpryadkin</a>
+          </div>
+          <div class="link-email">
+            <img src="/src/assets/at-thin-svgrepo-com.svg" alt="email" width="33">
+            <a href="/">maxpryadkin@gmail.com</a>
+          </div>
+        </div>
+      </div>
+      <div class="image">
+        <img class="logo" src="/src/assets/zaeEEWR.jpg" alt="Логотип пмс" width="136">
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="about">
+      <h2>Обо мне</h2>
+      <p>
+        2010 году заинтересовался Web. Изучал как сделал сайт и нашел WordPress.<br>
+        2011 начинаю работать Seo специалистом. Разработкой занимаюсь как хобби.
+      </p>
+      <p>
+        2020 изучаю JS и фреймворки хочу полностью уйти в разработку.<br>
+        Сейчас пишу на Vue.js ищу компанию для развития и долгосрочной работы.<br>
+        Хочу ходить в офис, общаться с коллегами, развиваться в программировании.
+      </p>
+    </div>
+    <div class="portfolio">
+      <h2>Портфолио</h2>
+      <ul>
+        <li>
+          <img src="/src/assets/github-logo-thin-svgrepo-com.svg" alt="github" width="36">
+          <p><a href="https://github.com/Lahma-tut">github.com</a></p>
+        </li>
+      </ul>
+      
+    </div>
+    <div class="jobs">
+      <h2>Кем работал</h2>
+      <ul>
+        <li>
+            <img src="/src/assets/drop-half-thin-svgrepo-com.svg" alt="email" width="36">
+            <p>2019 - Н.В. SEO—специалист, Менеджер проекта<br>
+              "Премьер—Агро"
+              <a href="https://pr-agro.ru/">www.pr—agro.ru</a>
+            </p>
+        </li>
+        <li>
+            <img src="/src/assets/bug-beetle-thin-svgrepo-com.svg" alt="email" width="36">
+            <p>2014 — 2019 SEO—специалист<br>
+              "ЗемлякоФФ защита растений Центр"
+              <a href="https://zzrc.ru/">www.zzrc.ru</a>
+            </p>
+        </li>
+        <li>
+            <img src="/src/assets/sketch-logo-thin-svgrepo-com.svg" alt="email" width="36">
+            <p>2012—2014 SEO—специалист<br>
+              "Evrone"
+              <a href="https://evrone.ru/">www.evrone.ru</a>
+            </p>
+        </li>
+        <li>
+            <img src="/src/assets/snowflake-thin-svgrepo-com.svg" alt="email" width="36">
+            <p>2011—2012 SEO—специалист<br>
+              "Арктическая Лаборатория"
+              <a href="https://arcticlab.ru/">www.arcticlab.ru</a>
+            </p>
+        </li>
+      </ul>
+    </div>
+  </main>
+  <footer>
+    <div class="wrapper">
+      <p>&copy; 2024 Vue.js Максим Прядкин</p>
+    </div>
+  </footer>
+</template>
